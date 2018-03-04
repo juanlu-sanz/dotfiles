72734cd15b05f1797b0f4cf3d2c69cb2326b568f
Juan Luis Sanz <juanlu-sanz@users.noreply.github.com>
2018-03-04 19:03:30 +0100

    Update tmux.zsh

--- zsh/tmux.zsh
+++ zsh/tmux.zsh
@@ -1,4 +1,4 @@
 # Launch a default tmux session
 if command -v tmux>/dev/null; then
-  [[ ! $TERM =~ screen ]] && [ -z $TMUX ] && exec tmux new -A
+  [[ ! $TERM =~ screen ]] && [ -z $TMUX ] && exec tmux attach || tmux new
 fi
